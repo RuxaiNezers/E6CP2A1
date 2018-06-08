d8802bb87aee1195899b3efcab6a0466a4972272
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-07 21:26:55 -0400

    2.1 solved

--- 2 arrays/ejercicio1.rb
+++ 2 arrays/ejercicio1.rb
@@ -5,4 +5,21 @@
 # 4. Mostrar en pantalla todos los elementos junto con un índice.
 # 5. Mostrar en pantalla todos los elementos que se encuentren en una posición par.
 
-arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+arreglo = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+puts arreglo.first
+puts arreglo.last
+
+arreglo.each do |valor|
+	puts valor
+end
+
+arreglo.each_with_index do |valor, indice|
+	puts "#{indice} = #{valor}"
+end
+
+arreglo.each_with_index do |valor,indice|
+	if indice.even?
+		puts valor
+	end
+end
