8d4ef6f15e9ce9375994d6e8828ac79a96aeeea4
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-07 20:55:31 -0400

    1.5 solved

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,16 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+
+def pares(numx, numy)
+  if numx<numy
+  	for	a in numx..numy
+      puts a if (a % 2).zero?
+    end
+  else 
+    for	b in numy..numx
+      puts b if (b % 2).zero?
+    end
+  end
+end
+
+pares(3, 20)
