0987422d3eb20bf9a8dc3beb65c60b24bb433905
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-10 23:40:22 -0400

    2.3 solved

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -4,4 +4,49 @@
  # 3. Crear un método para obtener el promedio de un arreglo.
  # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 
-a = [1,2,3,9,1,4,5,2,3,6,6]
+
+
+def eliminarPares(arre)
+  arre.delete_if { |valor| valor.even? }
+
+  arre 
+end
+
+def sumaArreglo(arre)
+	suma = 0
+  arre.each do |valor|
+    suma += valor
+  end
+  suma
+end
+
+def promedioArreglo(arre)
+	suma = 0
+  arre.each do |valor|
+  	suma += valor
+  end
+
+  conteo = arre.count
+  promedio = (suma.to_f/conteo)
+  promedio
+end
+
+def valorMasUno(arre, arrN)
+  arre.each_with_index do |valor, indice|
+    arrN.push(arre[indice]+1)
+  end
+
+  arrN
+end
+
+arr = [1,2,3,9,1,4,5,2,3,6,6]
+arrN = []
+suma =0
+
+print eliminarPares(arr)
+puts ""
+print sumaArreglo(arr)
+puts ""
+print promedioArreglo(arr)
+puts ""
+print valorMasUno(arr, arrN)
